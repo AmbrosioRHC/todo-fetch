a5c994d3b7d7ba6dc95fe03857a9ce9b2045f11d
Ambrosio <ambrosio.rhc@gmail.com>
2024-03-07 19:48:37 -0300

    cotinuando la edición

--- src/components/tareas.jsx
+++ src/components/tareas.jsx
@@ -1,14 +1,17 @@
-import {useState} from "react";
+import { useState } from "react";
 
 const Tareas = () => {
     return (
         <>
-        <div className="tareaAgregada">Ir al super</div>
-        <div className="tareaAgregada">Pasear al perro</div>
-        <div className="tareaAgregada">Lavar ropa</div>
-        <div className="itemsLeft">quedan x items</div>
+            <div className="tareas">
+                <div className="tareaAgregada">Ir al super</div>
+                <div className="tareaAgregada">Pasear al perro</div>
+                <div className="tareaAgregada">Lavar ropa</div>
+                <div className="itemsLeft">quedan x items</div>
+            </div>
+
         </>
-        
+
     )
 }
 
