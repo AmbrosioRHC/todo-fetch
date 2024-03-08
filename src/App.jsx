9554a4ed4eff82ff0e70411e0117b1aa35727ff8
Ambrosio <ambrosio.rhc@gmail.com>
2024-03-08 19:08:33 -0300

    0.1

--- src/App.jsx
+++ src/App.jsx
@@ -6,32 +6,37 @@ import Tareas from "./components/tareas";
 
 function App() {
 
+  // useState  guarda y actualiza lo ingresado por el usuario
+
   const [inputText, setInputText] = useState({});
-  const [tarea, setTarea] = useState([
-    { inputOfTodo: "" },
 
-  ])
-
+// Esto no lo entiendo:
+  const [tarea, setTarea] = useState([])
+  // Acá e.target.name toma el name="nameOfInput" 
+  // y e.target.value lo ingresado por el usuario
   const handleChange = (e) => {
     setInputText({
       ...inputText,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value 
     })
-    console.log("e.target.value", e.target.value)
+    console.log("e.target.value ", e.target.value, "e.target.name ", e.target.name)
   }
-
+// la variable tarea será utilizada en el map para ir creando las tareas ingresadas
   const handleSubmit = (e) => {
     e.preventDefault()
     setTarea([...tarea, inputText])
 
   }
+
+// En form tengo que poner onSubmit={handleSubmit}??
+
   return (
     <>
       <div className="caja">
         <h1 className="todo">To-do list</h1>
         <div className="card">
           <div className="textInput" >
-            <form onSubmit={handleSubmit}>
+              <form >
               <input
                 onChange={handleChange}
                 className="inputTareas"
--- src/components/tareas.jsx
+++ src/components/tareas.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
 
-const Tareas = ({tareaProps}) => {
+const Tareas = (props) => {
     return (
         <>
             <div className="tareas">
-                <div className="tareaAgregada">{tareaProps.nameOfInput}</div>
+                <div className="tareaAgregada">{props.tareaProps.nameOfInput}</div>
                 {/* <div className="tareaAgregada">Pasear al perro</div>
                 <div className="tareaAgregada">Lavar ropa</div>
                 <div className="itemsLeft">quedan x items</div> */}
